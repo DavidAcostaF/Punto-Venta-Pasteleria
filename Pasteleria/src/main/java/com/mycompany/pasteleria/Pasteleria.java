5b2c34e0d27d62cfaf3021306877d92e29553c04
David <acostafajardodavid@gmail.com>
2024-03-18 15:23:23 -0700

    Creacion de clase control

--- Pasteleria/src/main/java/com/mycompany/pasteleria/Pasteleria.java
+++ Pasteleria/src/main/java/com/mycompany/pasteleria/Pasteleria.java
@@ -1,16 +1,18 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.pasteleria;
+
+import gui.MenuPrincipal;
 
 /**
  *
  * @author af_da
  */
 public class Pasteleria {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        MenuPrincipal mp = new MenuPrincipal();
+        mp.setVisible(true);
     }
 }
