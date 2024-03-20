37660cfda31147d71d87f6989e0852844b6fd0eb
David <acostafajardodavid@gmail.com>
2024-03-19 22:48:18 -0700

    Si

--- Pasteleria/src/main/java/gui/DlgAgregarPastel.java
+++ Pasteleria/src/main/java/gui/DlgAgregarPastel.java
@@ -18,7 +18,7 @@ import javax.swing.DefaultComboBoxModel;
 public class DlgAgregarPastel extends javax.swing.JDialog {
 
     private List<ProductoDTO> pasteles;
-    
+
     /**
      * Creates new form DlgAgregarPastel
      */
@@ -156,19 +156,18 @@ public class DlgAgregarPastel extends javax.swing.JDialog {
         // TODO add your handling code here:
     }//GEN-LAST:event_jComboBox1ActionPerformed
 
-    
-    private void desplegarPasteles(List<ProductoDTO> lista){
+    private void desplegarPasteles(List<ProductoDTO> lista) {
         jComboBox1.setModel(new PastelComboBoxModel(lista));
     }
 
-    private void agregarPasteles(){
+    private void agregarPasteles() {
         pasteles.add(new ProductoDTO(300, "Pastel de Chocolate", "Cubierto con ganache de chocolate y decorado con virutas de chocolate.", "Grande"));
         pasteles.add(new ProductoDTO(200, "Tarta de Fresa", "Rellena con crema de fresa y cubierta con glaseado de fresa.", "Mediano"));
         pasteles.add(new ProductoDTO(150, "Pastel de Zanahoria", "Lleno de nueces y especias, cubierto con frosting de queso crema.", "Chico"));
         pasteles.add(new ProductoDTO(300, "Tarta de Limón", "Bizcocho esponjoso de limón cubierto con crema de limón y ralladura de limón.", "Grande"));
         pasteles.add(new ProductoDTO(400, "Pastel Red Velvet", "Bizcocho de terciopelo rojo con capas de frosting de queso crema.", "Grande"));
     }
-    
+
     /**
      * @param args the command line arguments
      */
