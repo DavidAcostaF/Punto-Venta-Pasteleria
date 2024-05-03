f6381bda056c9749d9594a76b56f269ad619dee8
David <acostafajardodavid@gmail.com>
2024-05-03 03:09:45 -0700

    Subsistemas agregar ingrediente y consultar ingredientes

---
+++ PasteleriaConsultarIngredientes/src/main/java/com/mycompany/pasteleriaconsultaringredientes/PasteleriaConsultarIngredientes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pasteleriaconsultaringredientes;
+
+/**
+ *
+ * @author af_da
+ */
+public class PasteleriaConsultarIngredientes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
