aefba1448030763ec367850abe7159bc9817a9c8
David <acostafajardodavid@gmail.com>
2024-05-03 00:09:01 -0700

    Proyectos creados caso de uso inventario ingredientes

---
+++ PasteleriaNegocio/src/main/java/com/mycompany/pastelerianegocio/IInventarioIngredientesBO.java
@@ -0,0 +1,14 @@
+package com.mycompany.pastelerianegocio;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author af_da
+ */
+public interface IInventarioIngredientesBO {
+    
+}
