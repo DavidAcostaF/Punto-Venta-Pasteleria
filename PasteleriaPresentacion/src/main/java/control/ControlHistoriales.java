4d0b63818dfb5c0aadec396f59d15425a161c5ea
abel11604 <abelcorona.quintero@gmail.com>
2024-05-06 23:01:47 -0700

    Frames de caso de uso de historiales y clase control

--- PasteleriaPresentacion/src/main/java/control/ControlHistoriales.java
+++ PasteleriaPresentacion/src/main/java/control/ControlHistoriales.java
@@ -4,10 +4,31 @@
  */
 package control;
 
+import presentacion.Presentacion_FrmDetallesVenta;
+import presentacion.Presentacion_FrmHistorialVentas;
+import presentacion.Presentacion_FrmMenuHistoriales;
+
 /**
  *
  * @author abelc
  */
 public class ControlHistoriales {
+     public ControlHistoriales() {
+    }
     
+    public void mostrarHistorialVentas() {
+        Presentacion_FrmHistorialVentas historialVentas = new Presentacion_FrmHistorialVentas();
+        historialVentas.mostrarHistorialVentas();
+    }
+    
+    public void mostrarMenu() {
+        Presentacion_FrmMenuHistoriales menu = new Presentacion_FrmMenuHistoriales();
+        menu.setVisible(true);
+    }
+    
+    public void mostrarDetallesVenta(){
+    Presentacion_FrmDetallesVenta detallesVenta=new Presentacion_FrmDetallesVenta();
+    detallesVenta.mostrarDetallesVentas();
+    
+    }
 }
---
+++ PasteleriaPresentacion/src/main/java/extras/TableActionEvent.java
@@ -0,0 +1,13 @@
+package extras;
+
+/**
+ *
+ * @author RAVEN
+ */
+public interface TableActionEvent {
+
+
+    public void onDelete(int row);
+
+    public void onView(int row);
+}
