b6cb3279469d67687b835ae21e2a3d179480391d
David Acosta Fajardo <108051764+DavidAcostaF@users.noreply.github.com>
2024-05-07 23:47:45 -0700

    Ingrediente detalle creado

--- PasteleriaPersistencia/src/main/java/com/mycompany/pasteleriadocumentosanidados/IngredienteDetalle.java
+++ PasteleriaPersistencia/src/main/java/com/mycompany/pasteleriadocumentosanidados/IngredienteDetalle.java
@@ -9,5 +9,40 @@ package com.mycompany.pasteleriadocumentosanidados;
  * @author af_da
  */
 public class IngredienteDetalle {
+
+    private String ingredienteId;
+    private String nombre;
+    private Integer cantidad;
+
+    public IngredienteDetalle(String ingredienteId, String nombre, Integer cantidad) {
+        this.ingredienteId = ingredienteId;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+
+    public String getIngredienteId() {
+        return ingredienteId;
+    }
+
+    public void setIngredienteId(String ingredienteId) {
+        this.ingredienteId = ingredienteId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+    
     
 }
