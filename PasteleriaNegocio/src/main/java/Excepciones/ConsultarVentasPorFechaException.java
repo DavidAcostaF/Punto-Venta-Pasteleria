63d5050a30086185384c30aa4120c72aa99ad972
David Acosta Fajardo <108051764+DavidAcostaF@users.noreply.github.com>
2024-05-15 05:43:41 -0700

    Documentacion negocio

--- PasteleriaNegocio/src/main/java/Excepciones/ConsultarVentasPorFechaException.java
+++ PasteleriaNegocio/src/main/java/Excepciones/ConsultarVentasPorFechaException.java
@@ -1,11 +1,19 @@
 package Excepciones;
 
 /**
- *
+ * La clase ConsultarVentasPorFechaException representa una excepción que se lanza
+ * cuando ocurre un error al intentar consultar las ventas por fecha.
+ * 
  * @author PC
  */
 public class ConsultarVentasPorFechaException extends Exception {
 
+    /**
+     * Constructor que permite crear una instancia de ConsultarVentasPorFechaException
+     * con un mensaje de error personalizado.
+     * 
+     * @param message El mensaje de error que describe la causa de la excepción.
+     */
     public ConsultarVentasPorFechaException(String message) {
         super(message);
     }
