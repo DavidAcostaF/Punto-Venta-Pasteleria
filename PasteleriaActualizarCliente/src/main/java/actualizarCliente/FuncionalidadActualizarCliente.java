56387b332ad76e7dd71dfd2df211672db8e74633
hectorbaez247283 <159949506+hectorbaez247283@users.noreply.github.com>
2024-05-14 21:00:14 -0700

    PasteleriaActualizarCliente subida

---
+++ PasteleriaActualizarCliente/src/main/java/actualizarCliente/FuncionalidadActualizarCliente.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package actualizarCliente;
+
+import com.mycompany.pastelerianegocio.ClientesBO;
+import com.mycompany.pastelerianegocio.IClientesBO;
+import dto.DTO_Cliente;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class FuncionalidadActualizarCliente implements IFuncionalidadActualizarCliente {
+
+    IClientesBO clientesBO;
+
+    public FuncionalidadActualizarCliente() {
+        this.clientesBO = new ClientesBO();
+    }
+    
+    @Override
+    public DTO_Cliente actualizarCliente(DTO_Cliente cliente) {
+        DTO_Cliente dtoCliente = clientesBO.actualizarCliente(cliente);
+        return dtoCliente;
+    }
+    
+}
---
+++ PasteleriaActualizarCliente/src/main/java/actualizarCliente/IFuncionalidadActualizarCliente.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package actualizarCliente;
+
+import dto.DTO_Cliente;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public interface IFuncionalidadActualizarCliente {
+    public DTO_Cliente actualizarCliente(DTO_Cliente cliente);
+}
